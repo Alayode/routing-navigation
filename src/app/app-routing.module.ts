23afdea76aa584be967bced39b6d36a3a3c0b9f3
Chris Samuel <chrisk.samueljr@gmail.com>
2019-06-30 18:16:12 -0400

    configured the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+	{ path: 'crisis-center', component: CrisisListComponent },
+	{ path: 'hero/:id',      component: HeroDetailComponent },
+	{
+		path: 'heroes',
+		component: HeroListComponent,
+		data: { title: 'Heroes List'}
+	},
+	{
+		path: '',
+		redirectTo: '/heroes',
+		pathMatch: 'full'
+	},
+
+	// The ** Path in this route is our wildCard catching component
+	// The router will seelct this route IF the requested URL Doesn't
+	// match any paths for the routes defined earlier in our configuration
+	// This is very useful if you would like to design a cool 404- Not Found
+	// page or redirecting to another route.
+	{
+	    path: '**', 
+	    component: PageNotFoundComponent 
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+  		RouterModule.forRoot(routes,
+  		{ enableTracing: true } // debugging Purposes only
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent,
+    HeroDetailComponent,
+    CrisisListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
