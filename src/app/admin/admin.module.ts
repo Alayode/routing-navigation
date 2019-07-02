698cae2b03696b91fb9bc59225c73a244bd327b1
Chris Samuel <chrisk.samueljr@gmail.com>
2019-07-02 13:15:00 -0400

    generated the adminModule and added its config

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ManageCrisisComponent } from './manage-crises/manage-crises.component';
+import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  					AdminDashboardComponent, 
+  					AdminComponent, 
+  					ManageCrisisComponent,
+  					ManageHeroesComponent
+  				],
   imports: [
     CommonModule,
     AdminRoutingModule
