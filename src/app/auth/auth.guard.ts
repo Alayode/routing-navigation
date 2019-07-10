92db8cefb8029c7b13f9898d4c28cc7ed427064e
Chris Samuel <chrisk.samueljr@gmail.com>
2019-07-10 15:32:57 -0400

    use the AuthService to check if the user is loggedIn

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,14 +2,38 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { AuthService } from 'auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements  {
+	
+	constructor(private authService: AuthService,
+	            private router: Router ) {}
+
+
 	canActivate(
          next: ActivatedRouteSnapShot,
          state: RouterStateSnapshot
 		): boolean {
 		console.log('AuthGuard#canActivate called');
+		return this.checkLogin(url);
 	} 
+
+	checkLogin(url: string): boolean {
+		if ( this.authService.isLoggedIn )  { return true; }
+
+		// Store the attempted URL for redirecting 
+		this.authService.redirectUrl = url;
+
+
+		// Navigate tot he login page with extras 
+		this.router.navigate(['/login']);
+
+		return false;
+
+	}
+
+
 }
