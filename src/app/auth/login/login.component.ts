d6a139a97532bc720cd5bc269f3440925c90181a
Chris Samuel <chrisk.samueljr@gmail.com>
2019-07-10 16:11:07 -0400

    added the LoginComponent

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	message: string;
 
-  ngOnInit() {
+
+  constructor(public authService: AuthService, public router: Router) { 
+  	this.setMessage();
   }
+
+	setMessage() {
+	  	this.message = ' Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+	  }
+
+	  login() {
+	  	this.message = ' Trying to log in  ...';
+	  
+	  	this.authService.login().subscribe(() => { 
+	  		this.setMessage();
+
+	  		if (this.authService.isLoggedIn) { 
+	  			// Get the redirect URL from our auth service
+
+	  			// if no redirect has been set, use the default
+
+	  			let redirect = this.authService.redirectUrl ?
+
+	  				this.router.parseUrl(this.authService.redirectUrl) : '/admin'
+
+	  				// Redirect te user 
+	  				this.router.navigateByUrl(redirect);
+
+
+	  		}
+	  	});
+
+	  	logout() {
+	  		this.authService.logout();
+	  		this.setMessage();
+	  	}
+
+	  }
 
 }
